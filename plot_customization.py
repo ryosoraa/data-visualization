e75bc6582ac3c892f9d22455a8bae3b2fea8b1b1
ryosyntaxx <riodwi12174@gmail.com>
2023-09-04 23:37:04 +0700

    add plot_customization

---
+++ plot_customization.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+income = [55, 56, 78, 79, 83, 84, 90, 94]
+
+income_ticks = list(range(50, 100, 2))
+
+plt.plot(years, income)
+plt.title('income in year', fontsize=20, fontname='verdana')
+plt.xlabel('years')
+plt.ylabel('income')
+plt.yticks(income_ticks, [f'Rp {x}k' for x in income_ticks])
+plt.show()
