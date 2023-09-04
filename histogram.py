45f52bfefeaff56707464aafdecd5e297c4c9156
ryosyntaxx <riodwi12174@gmail.com>
2023-09-04 22:34:50 +0700

    update histogram

--- histogram.py
+++ histogram.py
@@ -3,7 +3,7 @@ import numpy as np
 
 ages = np.random.normal(20, 1.5, 1000)
 
-plt.hist(ages, bins=20)
+plt.hist(ages, bins=20, cumulative=True)
 
 # bins in hist can like = plt.hist(ages, bins=[ages.min(), ages.max()])
 plt.show()
