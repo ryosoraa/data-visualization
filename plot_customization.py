0752d7115aa01bcd6be38aea50d954ae071d500b
ryosyntaxx <riodwi12174@gmail.com>
2023-09-06 10:44:49 +0700

    update comment and fiture

--- plot_customization.py
+++ plot_customization.py
@@ -8,7 +8,13 @@ income_ticks = list(range(50, 100, 2))
 
 plt.plot(years, income)
 plt.title('income in year', fontsize=20, fontname='verdana')
+# fontsize is used to set the font size
+# fontname is used to set the type of font used
+
 plt.xlabel('years')
 plt.ylabel('income')
 plt.yticks(income_ticks, [f'Rp {x}k' for x in income_ticks])
+
+# yticks are used to assign values ​​to the left of the data table
+
 plt.show()
