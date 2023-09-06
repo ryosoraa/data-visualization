b92a59e4fcc0689dcfc0bd1c1fcaee55e423d375
ryosyntaxx <riodwi12174@gmail.com>
2023-09-06 10:27:27 +0700

    add create example

---
+++ create_example.py
@@ -0,0 +1,14 @@
+from numpy.random import randint
+
+data_a = []
+data_b = []
+data_c = []
+
+for a in range(10):
+    data_a.append(randint(90, 200))
+    data_b.append(randint(90, 200))
+    data_c.append(randint(90, 200))
+
+print(data_a)
+print(data_b)
+print(data_c)
